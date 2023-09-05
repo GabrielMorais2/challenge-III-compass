ffc90899999ddc34815f07ed295ffac00b1c0675
Gabriel Moraes <moraisgabriel274@gmail.com>
2023-09-04 22:53:56 -0300

    test(ms-cars): created tests for CarService class, 100% coverage.

--- ms-cars/src/test/java/com/moraes/gabriel/mscars/MsCarsApplicationTests.java
+++ ms-cars/src/test/java/com/moraes/gabriel/mscars/MsCarsApplicationTests.java
@@ -3,7 +3,6 @@ package com.moraes.gabriel.mscars;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MsCarsApplicationTests {
 
 	@Test
