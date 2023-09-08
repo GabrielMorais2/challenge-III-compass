44ec9322e4c286f3281d413e9d5e58dec8f6e36b
Gabriel Moraes <moraisgabriel274@gmail.com>
2023-09-07 21:23:01 -0300

    feat(ms-races): implemented swagger to document the api and add summary to microservice endpoints

--- ms-races/src/main/java/com/moraes/gabriel/msraces/domain/Race/RaceController.java
+++ ms-races/src/main/java/com/moraes/gabriel/msraces/domain/Race/RaceController.java
@@ -1,5 +1,6 @@
 package com.moraes.gabriel.msraces.domain.Race;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,11 @@ public class RaceController {
 
     private final RacesService racesService;
 
+
+    @Operation(
+            summary = "Start a Race",
+            description = "Initiate a new race with the provided race request data. The race will be carried out automatically."
+    )
     @PostMapping("/start")
     public ResponseEntity<String> startRaces(@Valid @RequestBody RaceRequest raceRequest){
         racesService.runRaces(raceRequest);
