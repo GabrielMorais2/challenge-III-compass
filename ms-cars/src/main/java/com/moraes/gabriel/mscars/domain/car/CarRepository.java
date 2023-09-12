736bc20475e9dcecd9ecbca073d33b252b8a6c0d
Gabriel Moraes <moraisgabriel274@gmail.com>
2023-09-12 11:23:06 -0300

    rafactor(ms-cars): organized imports

--- ms-cars/src/main/java/com/moraes/gabriel/mscars/domain/car/CarRepository.java
+++ ms-cars/src/main/java/com/moraes/gabriel/mscars/domain/car/CarRepository.java
@@ -3,8 +3,6 @@ package com.moraes.gabriel.mscars.domain.car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
-
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 
