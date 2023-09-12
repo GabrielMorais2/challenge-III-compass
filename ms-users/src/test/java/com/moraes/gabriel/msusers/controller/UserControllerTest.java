0a0d22aff40f27f0fdcbd3288d38bf196d0e97ad
Gabriel Moraes <moraisgabriel274@gmail.com>
2023-09-11 22:59:03 -0300

    refactor(ms-history): organized imports.

--- ms-users/src/test/java/com/moraes/gabriel/msusers/controller/UserControllerTest.java
+++ ms-users/src/test/java/com/moraes/gabriel/msusers/controller/UserControllerTest.java
@@ -8,7 +8,6 @@ import com.moraes.gabriel.msusers.model.payload.UserResponse;
 import com.moraes.gabriel.msusers.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,7 +17,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
